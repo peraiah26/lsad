25e686e1aa68c3a2cf22578a55bd919077349194
name <peraiahtellagorla92@gmail.com>
2026-06-17 11:22:47 +0530

    ansible

---
+++ 10-condition.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+NUMBER=$1
+
+if [ NUMBER -lt 20 ]; then
+
+    echo " THE GIVEN NUMBER $NUMBER IS LESS THAN 20"
+
+else if [ NUMBER -eq 20 ]; then
+
+    echo " THE GIVEN NUMBER $NUMBER IS EQUAL TO 20"
+
+else
+
+    echo " THE GIVEN NUMBER $NUMBER IS GREATER TAHN 20@

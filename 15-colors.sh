029c3c40617a3f7bee7ef14f2832d61912a68d42
name <peraiahtellagorla92@gmail.com>
2026-06-19 16:24:35 +0530

    docker

---
+++ 15-colors.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[33m"
+N="\e[0m"
+
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+
+    echo "ERROR:: PLEASE RUN THE SCCRIPT ROOT PRIVILLAGES"
+    exit 1
+
+fi
+
+validate()
+{
+if [ $1 -ne 0 ]; then
+
+    echo -e "package installation $2 is $R failure $N"
+    exit 1
+else
+
+    echo "package installation $2 is success $N"
+fi
+}
+ 
+if [ $? -ne 0 ]; then
+    dnf install nginx -y
+    validate $? "nginx"
+ else
+    echo -e "nginx is already exist....$Y SKIPPING $N"
+fi
+
+if [ $? -ne 0 ]; then
+    dnf install mysql -y
+    validate $? "mysql"
+else
+    echo -e "nginx is already exist....$Y SKIPPING $N"
+fi
+
+if [ $? -ne 0 ]; then
+    dnf install python -y
+    validate $? "python"
+else
+    echo -e "nginx is already exist....$Y SKIPPING $N"
+fi
+
+

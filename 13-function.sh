c752d16a025fe141d1b8b5f7cc9a9635f96f678a
name <peraiahtellagorla92@gmail.com>
2026-06-19 15:55:07 +0530

    docker

---
+++ 13-function.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+
+    echo "ERROR:: PLEASE RUN THE SCCRIPT ROOT PRIVILLAGES"
+    exit 1
+
+f1
+
+validate(){
+if [ $1 -ne 0 ]; then
+
+    echo "package installation $2 is failure"
+    exit 1
+else
+
+    echo "package installation $2 is success"
+fi
+}
+
+dnf install nginx -y
+validate() $? "nginx"
+
+dnf install mysql -y
+validate() $? "mysql"
+
+dnf install python -y
+validate() $? "python"
+
+
